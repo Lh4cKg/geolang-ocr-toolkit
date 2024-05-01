00a2bf1baaa85b25e0d2995177b2c9570a82c59b
Lasha Gogua <lh4ckg@gmail.com>
2024-05-01 15:37:57 +0400

    added matcher and search modules

---
+++ src/geolangocr/matcher.py
@@ -0,0 +1,84 @@
+import logging
+import pathlib
+import typing as typ
+
+
+from .conf import settings
+from .search import match
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseMatcher(object):
+
+    def __init__(self, search_keywords: typ.List[str] = None,
+                 save: bool = False):
+        self.save = save
+        if not search_keywords:
+            self.search_keywords = self.load_search_keywords()
+        else:
+            if not isinstance(search_keywords, list):
+                raise TypeError(f'`search_keywords` must be a list.')
+            self.search_keywords = search_keywords
+
+    def match(self):
+        raise NotImplementedError
+
+    @staticmethod
+    def load_search_keywords() -> typ.List[str]:
+        keywords_file = settings.INPUT_DIR / 'keywords.txt'
+        if keywords_file.exists() is False:
+            keywords_file.touch()
+            # raise FileNotFoundError(
+            #     f'`{keywords_file.name}` does not exists. '
+            #     f'must be added to `{settings.INPUT_DIR}`.'
+            # )
+        with open(keywords_file) as f:
+            return f.readlines()
+
+    @staticmethod
+    def save_file(filename: str, keyword: str) -> None:
+        output_dir: pathlib.Path = settings.INPUT_DIR / 'matches.txt'
+        if output_dir.exists() is False:
+            output_dir.touch()
+
+        with open(output_dir, 'a') as f:
+            filename, page = filename.rsplit('-', 1)
+            f.write(
+                f'ფაილი: {filename} - '
+                f'გვერდი: {page} - '
+                f'ნაპოვნი საძიებო სიტყვა: "{keyword}"\n'
+            )
+
+
+class Matcher(BaseMatcher):
+
+    def __init__(self, filename: str, text: str, save: bool = False,
+                 search_keywords: typ.List[str] = None) -> None:
+        super().__init__(search_keywords, save)
+        self.filename = filename
+        self.text = text
+        # TODO should be add exclude found journal logics
+
+    def match(self) -> typ.Tuple[bool, int]:
+        distance = 0
+        for keyword in self.search_keywords:
+            matched, distance = match(self.text, keyword)
+            if matched:
+                logger.info(f'This `{keyword}` matched in {self.filename} file.')
+                if self.save is True:
+                    self.save_file(self.filename, keyword)
+                return matched, distance
+
+        logger.info(f'Keywords does not matched in `{self.filename}` file.')
+        return False, distance
+
+
+class FileMatcher(BaseMatcher):
+
+    def __init__(self, input_folder: typ.Union[str, pathlib.Path],
+                 search_keywords: typ.List[str] = None) -> None:
+        super().__init__(search_keywords)
+        self.input_folder = input_folder
+        # TODO will be add logic
---
+++ src/geolangocr/search.py
@@ -0,0 +1,72 @@
+import typing as typ
+from fuzzywuzzy import fuzz
+
+from .conf import settings
+
+
+def distance(q1: str, q2: str, partial: bool = False) -> int:
+    """
+    Calculate the Levenshtein distance between two
+    :param q1:
+    :type q1:
+    :param q2:
+    :type q2:
+    :param partial:
+    :type partial:
+    :return:
+    :rtype:
+    """
+    if partial:
+        return fuzz.partial_ratio(q1.lower(), q2.lower())
+    return fuzz.token_set_ratio(
+        q1.lower(), q2.lower(),
+        full_process=settings.TOKEN_FULL_PROCESS
+    )
+
+
+def match(q1: str, q2: str, th: int = settings.LEVENSHTEIN_MATCH_THRESHOLD,
+          partial: bool = False) -> typ.Tuple[bool, int]:
+    dist = distance(q1, q2, partial)
+    return dist >= th, dist
+
+
+class KMPSearch:
+    """
+        KMP (Knuth–Morris–Pratt) search algorithm
+        Link: https://en.wikipedia.org/wiki/Knuth-Morris-Pratt_algorithm
+    """
+    @classmethod
+    def search(cls, text, pattern):
+        """
+        text - T
+        pattern - P
+        Return all the matching position of pattern string P in T
+        """
+        result = []
+        partial = cls.partial_match(pattern)
+        j = 0
+        for i in range(len(text)):
+            while j > 0 and text[i] != pattern[j]:
+                j = partial[j - 1]
+            if text[i] == pattern[j]:
+                j += 1
+            if j == len(pattern):
+                result.append(i - (j - 1))
+                j = partial[j - 1]
+
+        return result
+
+    @staticmethod
+    def partial_match(pattern):
+        """
+        Calculate partial match table (also known as "failure function")
+        """
+        result = [0]
+
+        for i in range(1, len(pattern)):
+            j = result[i - 1]
+            while j > 0 and pattern[j] != pattern[i]:
+                j = result[j - 1]
+            result.append(j + 1 if pattern[j] == pattern[i] else j)
+        return result
+
